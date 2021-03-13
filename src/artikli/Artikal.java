498f12ed5013b6b58caeb807b4ad6e63f52118d2
majaa12 <majacol98@gmail.com>
2021-03-13 22:11:05 +0100

    Dodata logicka kontrola u sve set metode

--- src/artikli/Artikal.java
+++ src/artikli/Artikal.java
@@ -2,40 +2,52 @@ package artikli;
 
 public class Artikal {
 
-	 private String naziv;
-	 private int sifra;
-	 private String opis;
-	 private int kolicina;
-	 
+	private String naziv;
+	private int sifra;
+	private String opis;
+	private int kolicina;
+
 	public String getNaziv() {
 		return naziv;
 	}
-	
+
 	public void setNaziv(String naziv) {
+		if (naziv == null) {
+			throw new RuntimeException("Naziv ne sme biti null!");
+		}
 		this.naziv = naziv;
 	}
-	
+
 	public int getSifra() {
 		return sifra;
 	}
-	
+
 	public void setSifra(int sifra) {
+		if (sifra < 0) {
+			throw new RuntimeException("Sifra mora biti veca od 0!");
+		}
 		this.sifra = sifra;
 	}
-	
+
 	public String getOpis() {
 		return opis;
 	}
-	
+
 	public void setOpis(String opis) {
+		if (opis == null) {
+			throw new RuntimeException("Opis artikla ne sme biti null!");
+		}
 		this.opis = opis;
 	}
-	
+
 	public int getKolicina() {
 		return kolicina;
 	}
-	
+
 	public void setKolicina(int kolicina) {
+		if (kolicina < 0) {
+			throw new RuntimeException("Kolicina ne sme biti manja od 0!");
+		}
 		this.kolicina = kolicina;
 	}
 
@@ -56,11 +68,11 @@ public class Artikal {
 			return true;
 		if (obj == null)
 			return false;
-		
+
 		if (getClass() != obj.getClass())
 			return false;
 		Artikal other = (Artikal) obj;
-		
+
 		if (sifra != other.sifra)
 			return false;
 		return true;
@@ -70,6 +82,5 @@ public class Artikal {
 	public String toString() {
 		return "Artikal [naziv=" + naziv + ", sifra=" + sifra + ", opis=" + opis + ", kolicina=" + kolicina + "]";
 	}
-	
-	
+
 }
--- src/artikli/KucnaHemija.java
+++ src/artikli/KucnaHemija.java
@@ -5,7 +5,7 @@ import java.util.GregorianCalendar;
 public class KucnaHemija extends Artikal {
 
 	private GregorianCalendar rokTrajanja;
-	
+
 	public KucnaHemija() {
 		this.rokTrajanja = new GregorianCalendar();
 	}
@@ -15,13 +15,22 @@ public class KucnaHemija extends Artikal {
 	}
 
 	public void setRokTrajanja(GregorianCalendar rokTrajanja) {
-		this.rokTrajanja = rokTrajanja;
+		if (rokTrajanja.before(new GregorianCalendar()))
+			throw new RuntimeException("Ovaj datum je vec prosao!");
+		if (!(rokTrajanja.MONTH >= 0 && rokTrajanja.MONTH <= 11))
+			throw new RuntimeException("Meseci se krecu o 0-11!");
+		if (rokTrajanja.YEAR <= 0)
+			throw new RuntimeException("Godina mora biti pozitivan broj!");
+		if (rokTrajanja.DAY_OF_MONTH <= 0 || rokTrajanja.DAY_OF_MONTH > 31)
+			throw new RuntimeException("Ne postoji ovaj dan u mesecu!");
+		
+			this.rokTrajanja = rokTrajanja;
 	}
 
 	@Override
 	public String toString() {
-		return "KucnaHemija: " + getNaziv() + ", sifra: " + getSifra() + ", opis: " + getOpis() +
-				", kolicina: " + getKolicina() + "rokTrajanja: " + getRokTrajanja();
+		return "KucnaHemija: " + getNaziv() + ", sifra: " + getSifra() + ", opis: " + getOpis() + ", kolicina: "
+				+ getKolicina() + "rokTrajanja: " + getRokTrajanja();
 	}
 
 	@Override
@@ -34,14 +43,14 @@ public class KucnaHemija extends Artikal {
 						return false;
 				} else if (!rokTrajanja.equals(other.rokTrajanja))
 					return false;
-				
-			} else return false;
-			
-		} else return false;
-		
+
+			} else
+				return false;
+
+		} else
+			return false;
+
 		return true;
 	}
-	
-}
 
-	
+}
