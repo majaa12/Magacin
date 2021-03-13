03c1327a403d8ac619fe4fafea3815468edd5ab7
majaa12 <majacol98@gmail.com>
2021-03-13 22:41:33 +0100

    Uvedena klasa Magacin koja implementira MagacinInterface

---
+++ src/artikli/Magacin.java
@@ -0,0 +1,31 @@
+package artikli;
+
+import java.util.LinkedList;
+
+import magacin.MagacinInterface;
+
+public class Magacin implements MagacinInterface{
+
+	private LinkedList<Artikal> artikli  = new LinkedList<>();
+	
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+	
+		
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+		
+		
+	}
+
+	@Override
+	public Artikal vratiArtikal(int sifra) {
+	
+		return null;
+	}
+
+	
+	
+}
